aaa087e1032271c014da9aca8933281550752677
borisov <borisov.norilsk@gmail.com>
2020-06-20 16:19:13 +0500

    intial commit

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import View from '@vkontakte/vkui/dist/components/View/View';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import Snackbar from '@vkontakte/vkui/dist/components/Snackbar/Snackbar';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import Icon24Error from '@vkontakte/icons/dist/24/error';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Home from './panels/Home';
+import Intro from './panels/Intro';
+
+const ROUTES = {
+	HOME: 'home',
+	INTRO: 'intro'
+}
+
+const STORAGE_KEYS = {
+	STATUS: 'status'
+}
+
+const App = () => {
+	const [activePanel, setActivePanel] = useState(ROUTES.INTRO);
+	const [fetchedUser, setUser] = useState(null);
+	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	const [userHasSeenIntro, setUserHasSeenIntro] = useState(false);
+	const [snackbar, setSnackbar] = useState(null);
+
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await bridge.send('VKWebAppGetUserInfo');
+			const storageData = await bridge.send('VKWebAppStorageGet', {
+				keys: Object.values(STORAGE_KEYS)
+			});
+			const data = {};
+			storageData.keys.forEach( ({key, value}) => {
+				try {
+					data[key] = value ? JSON.parse(value) : {};
+					switch (key) {
+						case STORAGE_KEYS.STATUS:
+							if (data[key].hasSeenIntro) {
+								setActivePanel(ROUTES.HOME);
+								setUserHasSeenIntro(true);
+							}
+							break;
+						default:
+							break;
+					}
+				} catch(err) {
+					setSnackbar(
+						<Snackbar
+							layout='vertical'
+							onClose={() => setSnackbar(null)}
+							before={
+								<Avatar style={ {backgroundColor: 'var(--dynamic-red)'} }>
+									<Icon24Error fill='#fff' width='14' height='14'/>
+								</Avatar>
+							}
+							duration={900}
+						>
+							Проблема получения данных из хранилища
+						</Snackbar>
+					)
+				}
+			});
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
+
+	const go = e => {
+		setActivePanel(e.currentTarget.dataset.to);
+	};
+
+	const viewIntro = async function () {
+		try {
+			await bridge.send('VKWebAppStorageSet', {
+				key: STORAGE_KEYS.STATUS,
+				value: JSON.stringify({
+					hasSeenIntro: true
+				})
+			})
+		} catch(error) {
+			setSnackbar(
+				<Snackbar
+					layout='vertical'
+					onClose={() => setSnackbar(null)}
+					before={
+						<Avatar style={ {backgroundColor: 'var(--dynamic-red)'} }>
+							<Icon24Error fill='#fff' width='14' height='14'/>
+						</Avatar>
+					}
+					duration={900}
+				>
+					Проблема отправки данных в хранилище
+				</Snackbar>
+			)
+		}
+	}
+
+	return (
+		<View activePanel={activePanel} popout={popout}>
+			<Home id={ROUTES.HOME} fetchedUser={fetchedUser} go={go} snackbarError={snackbar}/>
+			<Intro id={ROUTES.INTRO} go={go} snackbarError={snackbar}/>
+		</View>
+	);
+}
+
+export default App;
+
---
+++ src/panels/Intro.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { platform, IOS } from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
+import './Intro.css';
+
+const osName = platform();
+
+const Intro = props => (
+	<Panel id={props.id}>
+		<PanelHeader>
+			Challenger
+		</PanelHeader>
+		{props.snackbarError}
+	</Panel>
+);
+
+Intro.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	snackbarError: PropTypes.string
+};
+
+export default Intro;
